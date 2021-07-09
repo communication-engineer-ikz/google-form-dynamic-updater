962bc22e760f5a50c0143c948ecc6be053ba32a8
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-07-09 17:11:03 +0900

    回答結果を選択肢に設定する処理を追加

--- code.js
+++ code.js
@@ -1,3 +1,44 @@
-function myFunction() {
+/* 参考
+    * https://qiita.com/_xAGAx_/items/9bf13b020cb605908cf5
+    * https://yukaobu.wordpress.com/2016/10/23/googleform-2/
+*/
+const questionName = "参加されますか？";
+
+function updateGoogleForm(){
+
+    const answerSheets = SpreadsheetApp.openById("1BhJ057fY31KjNQl5MGEPc5Nn2A3YN-dWg3IDNu1UK9g");
+    const answerSheet = answerSheets.getSheetByName("フォームの回答 1");
+    const answerSheetLastRow = answerSheet.getLastRow();
+
+    let answers;
+    let choiceArray = [];
+
+    if (answerSheetLastRow > 1) {
+        const questionNames = answerSheet.getRange(1, 1, 1, answerSheet.getLastColumn()).getValues();
+        const colCount = questionNames[0].indexOf(questionName);    
+        answers = answerSheet.getRange(2, colCount + 1, answerSheetLastRow - 1).getValues();    
+    }
   
+    for (answer of answers) {
+
+        choiceArray.push(answer[0]);
+    }
+
+    console.log(choiceArray);
+
+    const form = FormApp.getActiveForm();
+    const items = form.getItems();
+
+    console.log(items);
+
+    items.forEach(function(item){
+
+        console.log(item);
+        console.log(item.getTitle());
+            
+        if (item.getTitle() === questionName) {
+
+            item.asMultipleChoiceItem().setChoiceValues(choiceArray);
+        }
+    });
 }
